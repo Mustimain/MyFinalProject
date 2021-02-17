a0567e6eaca8947f137b28ad6641160d01be8a1e
Mustafa Ceylan <mustiartt@gmail.com>
2021-02-18 00:10:23 +0300

    2.18.21 Güncel

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,10 +1,14 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -20,15 +24,16 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
+
+        [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
-            // business codes
+            //loglama
+            //cacheremove
+            //performance
+            //transaction
+            //yetkilendirme
 
-            if (product.ProductName.Length < 2)
-            {
-                //Magic strings
-                return new ErrorResult(Messages.ProductNameInvalid);
-            }
             _productDal.Add(product);
 
             return new SuccessResult(Messages.ProductAdded);
@@ -36,7 +41,7 @@ namespace Business.Concrete
 
         public IDataResult<List<Product>> GetAll()
         {
-             if (DateTime.Now.Hour == 22)
+             if (DateTime.Now.Hour == 5)
              {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
              }
